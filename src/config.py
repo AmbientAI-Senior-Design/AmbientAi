aa97d004d768fe85e35cdbe3695df96621d900f5
patrickmenendez29 <56135747+patrickmenendez29@users.noreply.github.com>
2024-01-31 15:42:20 -0500

    added config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 PORT = os.getenv('PORT')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
