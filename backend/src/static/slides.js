ce9c9a4fc19889ad62e52d408d38b2da1d144304
matthew-fitzgerald123 <122577658+matthew-fitzgerald123@users.noreply.github.com>
2024-04-15 14:48:16 -0400

    slides.js

---
+++ backend/src/static/slides.js
@@ -0,0 +1,164 @@
+export type SlideContent = {
+    duration: number;
+    content: string;
+    slideId: number;
+    index: number;
+}
+
+const slides: SlideContent[] = [
+        {
+        duration: 3000,
+        content: "Raytheon awarded $1.2 billion dollar deal with Germany for Patriot Missile Defense System",
+        slideId: 1,
+        index: 0,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The Patriot system is capable of intercepting different types of hostile attacks including cruise missiles, ballistic missiles and air-breathing missiles.",
+        slideId: 2,
+        index: 1,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The system currently deployed by 19 countries internationally, with more than 240 in active use.",
+        slideId: 3,
+        index: 2,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000, // Assuming each slide shows for 3 seconds
+        content: "Raytheon was established in 1922 in Cambridge, Massachusetts.",
+        slideId: 4,
+        index: 3,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Northrop Grumman is expanding after being awarded a $178 million US Naval contract.",
+        slideId: 5,
+        index: 4,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The Rocket Center location in West Virginia will see an increase in employees, as well as including transfers from within the corporation.",
+        slideId: 6,
+        index: 5,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "United States Senator Joe Manchin (D-West Virginia) spoke on the contract, noting that the site is one of only two sites in the nation that can produce rocket motor energetics and the only one that is a public-private parternship.",
+        slideId: 7,
+        index: 6,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Northrop Aircraft was founded in 1939, and renamed in 1958 to Northrop Corp. It became Northrop Grumman after the acquisition of Grumman Corp in 1994.",
+        slideId: 8
+        index: 7,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Lockheed Martin plans to open up new facility in Fort Smith, Arkansas.",
+        slideId: 9,
+        index: 8,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The defense industry giant is the first known company to open a facility to support the foreign pilot training center being established at Ebbing Air National Guard Base.",
+        slideId: 10,
+        index: 9,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Lockheed Corp was founded in 1926, and Martin Marietta was founded in 1961. In 1995, Lockheed Corp and Martin Marietta merged, becoming Lockheed Martin.",
+        slideId: 11,
+        index: 10,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "L3 Harris to cut 50 jobs at Waco facility.",
+        slideId: 12,
+        index: 11,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "There is expected to be 1025 employees remaining afterwards.",
+        slideId: 13,
+        index: 12,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "In 1895, the Harris company of L3 Harris was founded as The Harris Automatic Press Company by Alfred S. Harris. L3 was founded in 1997 by Frank Lanza and Robert LaPenta. L3 and Harris Corp merged in 2018.",
+        slideId: 14,
+        index: 13,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The microwave was invented by Raytheon and started selling it in 1967.",
+        slideId: 15,
+        index: 14,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Lockheed Martin’s Martin Marietta company built the original ICBM (Intercontinental Ballistic Missile).",
+        slideId: 16,
+        index: 15,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The F-14 Tomcat in 1986’s Top Gun was built by Grumman.",
+        slideId: 17,
+        index: 1,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "The lunar module of the Apollo program was designed and assembled by Grumman corp.",
+        slideId: 18,
+        index: 17,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Marilyn Monroe once worked on Northrop’s aircraft production line.",
+        slideId: 19,
+        index: 18,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "In 1895, the Harris company of L3 Harris was founded as The Harris Automatic Press Company by Alfred S. Harris. L3 was founded in 1997 by Frank Lanza and Robert LaPenta.",
+        slideId: 20,
+        index: 19,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Harris shipped their first automatic press in 1897.",
+        slideId: 21
+        index: 20,
+        image: “path/to/image”,
+    },
+    {
+        duration: 3000,
+        content: "Harris relocated its corporate headquarters to Melbourne, Florida in 1978.",
+        slideId: 22.
+        index: 21,
+        image: “path/to/image”,
+    },
+];
+
