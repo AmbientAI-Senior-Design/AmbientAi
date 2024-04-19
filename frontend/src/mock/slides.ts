e48d661b186e09e173fc18460fa8c865ddf92394
matthew-fitzgerald123 <mattfitz0421@icloud.com>
2024-04-18 23:02:20 -0400

    done (i think)

--- frontend/src/mock/slides.ts
+++ frontend/src/mock/slides.ts
@@ -28,5 +28,71 @@ export const mockSlides: SlideCollection[] = [
             
         ]
     },
+    {
+        slideId: 2,
+        slides: [
+            {
+                slideId: 2,
+                index: 0,
+                duration: 1000,
+                src: host + "ambientAICompanyPage.png"
+            },
+            {
+                slideId: 2,
+                index: 1,
+                duration: 2000,
+                src: host + "whatAmbientAI.png"
+            },
+            {
+                slideId: 2,
+                index: 2,
+                duration: 2000,
+                src: host + "whyAmbientAI.png"
+            },
+            {
+                slideId: 2,
+                index: 3,
+                duration: 2000,
+                src: host + "howAmbientAI.png"
+            },
+        ]
+    },
+    {
+        slideId: 3,
+        slides: [
+            {
+                slideId: 3,
+                index: 0,
+                duration: 1000,
+                src: host + "flTechFactsTitle.png"
+            },
+            {
+                slideId: 3,
+                index: 1,
+                duration: 2000,
+                src: host + "Space.png"
+            },
+            {
+                slideId: 3,
+                index: 2,
+                duration: 2000,
+                src: host + "NotableAlumni.png"
+            },
+            {
+                slideId: 3,
+                index: 3,
+                duration: 2000,
+                src: host + "CompSci.png"
+            },
+            {
+                slideId: 3,
+                index: 4,
+                duration: 2000,
+                src: host + "Diversity.png"
+            },
+            
+            
+        ]
+    },
     
 ];
